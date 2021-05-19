fcf5002e1abe0cca2677ec2846026aca1d4b2ac2
Yuliya513 <yuliya.senik@gmail.com>
2021-05-19 16:48:05 +0300

    Initial commit

---
+++ src/com/company/Car.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Car {
+
+    public static void main(String[] args) {
+        private String type;
+        private int year;
+    }
+    public int get year(){
+        return year
+    }
+}
+    public int get Type(){
+    return Type
+}
+public class CarsSort{
+    public static void main(String args[])
+    Car[] cars=(New car ("Opel",2012));
+    Car[] cars=(New car ("Mercedes",1994));
+    Car[] cars=(New car ("Nissan",2008));
+    Sysytem.out.println("Original Array"+Array to String);
+      Sysytem.out.println("Opel");
+        Sysytem.out.println("Mercedes");
+          Sysytem.out.println("Nissan");
+    Arrays.sort
+
+
+}
+
+
